be1208b10a19a24179fd881f0a3eca3ae0a79a68
manhhung1994 <manhhungvoi@gmail.com>
2015-09-21 14:32:14 +0000

    updategemfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
   def hoten
-    render text: "Tu Manh Hung"
+    render text: "Tu Manh Hung || Lop : 7B Viet Nhat || MSVV: 20121877"
     
   end
   
